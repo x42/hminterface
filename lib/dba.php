204617fb6511186a105476f275639d9829b695c0
Robin Gareus <robin@gareus.org>
2012-01-13 20:34:08 +0100

    sanitize key parameter input.

--- lib/dba.php
+++ lib/dba.php
@@ -1,39 +1,13 @@
 <?php
 require_once(IHM_LIB.'data.php');
-
-/* dummy - test */
-function gen_item($key=0, $idx=0) {
-	return array(
-		'img'   => 'fakeimg.php?key='.$key.'&amp;idx='.$idx,
-		'key'   => $key,
-		'idx'   => $idx,
-		'title' => 'Title',
-		'date'  => 1960 + $idx*8,
-		'text'  => '<h2>So what.</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus magna in nibh mattis aliquam. Sed ut nibh at mauris ultrices molestie. Vestibulum ullamcorper dolor a dui vestibulum ut dapibus purus dignissim. Integer sit amet aliquet mauris. Mauris consequat urna et ligula mattis condimentum.
-</p>',
-	);
-}
-
-function testquery_data($key=0, $idx=0) {
-	$d=array (
-		/* top row - index item */  array (gen_item(1)),
-		/* middle row - 5 items */  array (
-			//gen_item(2,0), gen_item(2,1), gen_item(2,2), gen_item(2,3), gen_item(2,4)
-			null, gen_item(2,1), gen_item(2,2), gen_item(2,3), gen_item(2,4)
-		),
-		/* bottom row - index item */  array(gen_item(3,0)),
-	);
-
-	return $d;
-}
-
+# Data Base Access
 
 function keycircle($key, $add=0) {
 	global $keys;
 	$kcnt=count($keys);
 	$key+=$add;
+	if ($kcnt==0) return;
 
-	# TODO use modulo, permit empty $key list
 	while ($key<1) $key+=$kcnt;
 	while ($key>$kcnt) $key-=$kcnt;
 	return $key;
@@ -64,6 +38,7 @@ function keyitem($key, $item) {
 }
 
 function get_key_index($key) {
+	# start-item for each group. image closest to 2010
 	$diff = 1000000;
 	$idx  = 0;
 	$iter = 0;
@@ -78,12 +53,10 @@ function get_key_index($key) {
 }
 
 function get_key_index_old($key) {
-	# start-item for each group.
 	$c = count(get_items($key))-1;
 	if ($c>2) return $c-2;
 	return 0;
 }
-
 
 function get_items($key) {
 	global $data;
@@ -95,10 +68,18 @@ function get_item($key, $idx) {
 	return $items[$key];
 }
 
+function sanitize_key($key) {
+	global $keys;
+	if ($key<1) return 1;
+	if ($key>count($keys)) return count($keys);
+	return $key;
+}
+
 function query_data($key, $idx) {
-	#TODO check if item exists; valid key,idx
+	$key=sanitize_key(intval($key));
 
 	$rv=get_items($key);
+	$idx=$idx%count($rv);
 
 	if ($idx>1) {
 		$grps=array_slice($rv, $idx-2, 5, false);
