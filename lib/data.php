e62780d75c436f9abd7750a7fd2a66ec29a074b4
Robin Gareus <robin@gareus.org>
2012-01-13 17:47:43 +0100

    added date, missing images,.. style..

--- lib/data.php
+++ lib/data.php
@@ -17,23 +17,23 @@ $keys = array (
 $keyimg[1] = 'tel/Tel-1982-Mobira_Talkman_FB.jpg';
 $data[1] = array (
 array('date' => "1667", 'img'=> 'tel/Tel-1667-RobertHooke-ficelle_FB.jpg'),
-array('date' => "1876", 'img'=> 'tel/Tel-1876-Emetteuraliquide_Graham_Bell_FB.jpeg'),
+array('date' => "1876", 'img'=> 'tel/Tel-1876-Emetteuraliquide_Graham_Bell_FB.jpg'),
 array('date' => "1877", 'img'=> 'tel/Tel-1877-HandTelephone_G.Bell_FB.jpg'),
 array('date' => "1880", 'img'=> 'tel/Tel-1880_Ader_FB.jpg'),
 array('date' => "1892", 'img'=> 'tel/Tel-1892-Milde_FB.jpg'),
 array('date' => "1907", 'img'=> 'tel/Tel-1907_Aboilard_FB.jpg'),
-array('date' => "1910", 'img'=> 'tel/Tel-1910-Marty_mobileAOIP_FB.jpeg'),
-array('date' => "1924", 'img'=> 'tel/Tel-1924-PTT24_mobile_FB.jpeg'),
+array('date' => "1910", 'img'=> 'tel/Tel-1910-Marty_mobileAOIP_FB.jpg'),
+array('date' => "1924", 'img'=> 'tel/Tel-1924-PTT24_mobile_FB.jpg'),
 array('date' => "1926", 'img'=> 'tel/Tel-1926-AlexanderGraham_Bell_Wikipedia_FB.jpg'),
 array('date' => "1936", 'img'=> 'tel/Tel-1936-Motorola_MTS_FB.jpg'),
-array('date' => "1940", 'img'=> 'tel/Tel-1940-43ivoire_FB.jpeg'),
+array('date' => "1940", 'img'=> 'tel/Tel-1940-43ivoire_FB.jpg'),
 array('date' => "1943", 'img'=> 'tel/Tel-1943-PupitreOperateur_Bakelite_CIT_FB.jpg'),
 array('date' => "1950", 'img'=> 'tel/Tel-1950-Ericsson_Bakelite_Cadran_FB.jpg'),
 array('date' => "1956", 'img'=> 'tel/Tel-1956-MTA_Ericcson_FB.jpg'),
 array('date' => "1960", 'img'=> 'tel/Tel-1960-1990_filaire_cadran_Wikipedia_FB.jpg'),
 array('date' => "1962", 'img'=> 'tel/Tel-1962-IMTS_FB.jpg'),
 array('date' => "1965", 'img'=> 'tel/Tel-1965_MaxLaMenace_MaxwellSmart.jpg'),
-array('date' => "1975", 'img'=> 'tel/Tel-1975-filaire_clavier_FB.jpeg'),
+array('date' => "1975", 'img'=> 'tel/Tel-1975-filaire_clavier_FB.jpg'),
 array('date' => "1982", 'img'=> 'tel/Tel-1982-Dynatact_Motorola_(source_LePost.fr)_FB.jpg'),
 array('date' => "1982", 'img'=> 'tel/Tel-1982-Mobira_Talkman_FB.jpg'),
 array('date' => "2008", 'img'=> 'tel/Tel-2008-HologrammeCNN_FB.jpg'),
@@ -41,6 +41,11 @@ array('date' => "2009", 'img'=> 'tel/Tel-2009-lg-montre-telephone.jpg'),
 array('date' => "2010", 'img'=> 'tel/Tel-2010-PHILIPS_sansfil_CD1752B_Duo_FB.jpg'),
 array('date' => "2030", 'img'=> 'tel/Tel-2030-digitaltooth_f.4202_httpwww.petitpetitgamin.com.jpg'),
 array('date' => "2030", 'img'=> 'tel/Tel-2030-iphone-hologram_FB.jpg'),
+#array('date' => "", 'img'=> 'tel/Tel-PopPhone_FB.jpg'),
+#array('date' => "", 'img'=> 'tel/Tel-portevoix_FB.jpg'),
+#array('date' => "", 'img'=> 'tel/Tel-PortORotaryRED-03-Lima_FB.jpg'),
+#array('date' => "", 'img'=> 'tel/Tel-PortORotaryRED_FB.jpg'),
+array('date' => "2101", 'img'=> 'tel/Tel-Star_Wars_hologram_FB.jpg'),
 );
 
 $keyimg[2] = 'key/5amarina-clavier-pliable-1.jpg';
@@ -55,7 +60,7 @@ array('date' => "2000", 'img'=> 'key/7celluon-magic-cube.jpg'),
 );
 
 
-$keyimg[3] = 'scanner/09-Scanners.main.1990.jpg';
+$keyimg[3] = 'scanner/08-Scanners.tambour.1990-WA.jpg';
 $data[3] = array (
 array('date' => "1856", 'img'=> 'scanner/01-Pantelegraphe.1856-WA.jpg'),
 array('date' => "1908", 'img'=> 'scanner/02-Belinographe.1908-WA.jpg'),
@@ -70,18 +75,18 @@ array('date' => "1993", 'img'=> 'scanner/10-Scanners.tomodensitometrie.1993-WA.j
 array('date' => "2010", 'img'=> 'scanner/11-Scanner.Corporel.2010-WA.jpg'),
 );
 
-$keyimg[4] = 'volant/201005261403170909603.jpg';
+$keyimg[4] = 'volant/20110521143848675.jpg';
 $data[4] = array (
-array('date' => "", 'img'=> 'volant/volant-1893-Bretagne_esquisse.jpg'),
-array('date' => "", 'img'=> 'volant/volant-Le_Xe_à_XVIIe_siècle.jpg'),
-array('date' => "", 'img'=> 'volant/volant-1769-nicolas.jpg'),
-array('date' => "", 'img'=> 'volant/20090720185413-279915467.jpg'),
-array('date' => "", 'img'=> 'volant/201005261403170909603.jpg'),
-array('date' => "", 'img'=> 'volant/201005261403076211151.jpg'),
-array('date' => "", 'img'=> 'volant/20110521143848675.jpg'),
-array('date' => "", 'img'=> 'volant/20081113115185_2.jpg'),
-array('date' => "", 'img'=> 'volant/gt_hab10.jpg'),
-array('date' => "", 'img'=> 'volant/1302832116658_U4613P33DT20110414220146.jpg'),
+array('date' => "1769", 'img'=> 'volant/volant-1769-nicolas.jpg'),
+array('date' => "1850", 'img'=> 'volant/volant-Le_Xe_à_XVIIe_siècle.jpg'),
+array('date' => "1893", 'img'=> 'volant/volant-1893-Bretagne_esquisse.jpg'),
+array('date' => "1900", 'img'=> 'volant/20090720185413-279915467.jpg'),
+array('date' => "1910", 'img'=> 'volant/201005261403170909603.jpg'),
+array('date' => "1920", 'img'=> 'volant/201005261403076211151.jpg'),
+array('date' => "1950", 'img'=> 'volant/20110521143848675.jpg'),
+array('date' => "1990", 'img'=> 'volant/20081113115185_2.jpg'),
+array('date' => "2010", 'img'=> 'volant/gt_hab10.jpg'),
+array('date' => "2020", 'img'=> 'volant/1302832116658_U4613P33DT20110414220146.jpg'),
 );
 
 $keyimg[5] = 'camera/camera-1988-Digital.jpg';
@@ -129,7 +134,7 @@ array('date' => "2000", 'img'=> 'imprimante/6polaroid_pogo_3_screen_large.jpg'),
 
 $keyimg[8] = 'memoire/8-disquemagnétique-1977-Intel-SB.jpg';
 $data[8] = array (
-array('date' => "", 'img'=> 'memoire/1-Pap-VIII_eme_S-Chine-SB.jpg'),
+array('date' => "0", 'img'=> 'memoire/1-Pap-VIII_eme_S-Chine-SB.jpg'),
 array('date' => "1928", 'img'=> 'memoire/2_Carteperforee80_colonnes-1928-IBM-SB.jpg'),
 array('date' => "1948", 'img'=> 'memoire/3-memàtubedewilliams-1948-SB.jpg'),
 array('date' => "1949", 'img'=> 'memoire/4-bandemagnétique1949-SB.jpg'),
@@ -145,12 +150,12 @@ array('date' => "2001", 'img'=> 'memoire/11-cleusb-2001-SB.jpg'),
 
 $keyimg[9] = 'navigation/08-Google.Maps.2004-WA.jpg';
 $data[9] = array (
-array('date' => "", 'img'=> 'navigation/01-Boussole.IVavJC-WA.jpg'),
-array('date' => "", 'img'=> 'navigation/02-Astrolabe.IIavJC-WA.jpg'),
-array('date' => "", 'img'=> 'navigation/03-Baton.Jacob.XIV-WA.jpg'),
+array('date' => "-400", 'img'=> 'navigation/01-Boussole.IVavJC-WA.jpg'),
+array('date' => "-150", 'img'=> 'navigation/02-Astrolabe.IIavJC-WA.jpg'),
+array('date' => "1300", 'img'=> 'navigation/03-Baton.Jacob.XIV-WA.jpg'),
 array('date' => "1675", 'img'=> 'navigation/04-Chronometre.marine.1675-WA.jpg'),
 array('date' => "1730", 'img'=> 'navigation/05-Sextant.1730-WA.jpg'),
-array('date' => "", 'img'=> 'navigation/06-Instr.electr.nav.XX-WA.jpg'),
+array('date' => "1994", 'img'=> 'navigation/06-Instr.electr.nav.XX-WA.jpg'),
 array('date' => "1995", 'img'=> 'navigation/07-Dispositf.GPS.1995-WA.jpg'),
 array('date' => "2004", 'img'=> 'navigation/08-Google.Maps.2004-WA.jpg'),
 array('date' => "2005", 'img'=> 'navigation/09-Google.Earth.2005-WA.jpg'),
@@ -160,18 +165,18 @@ array('date' => "2010", 'img'=> 'navigation/10-Instr.Nav.Aerienne.XX-WA.jpg'),
 
 $keyimg[10] = 'projecteur/Projecteur_LCD-1989-EPSON_VJP-2000-7.jpg';
 $data[10] = array (
-array('date' => "", 'img'=> 'projecteur/Projecteur-trois_attraper-6.jpg'),
-array('date' => "", 'img'=> 'projecteur/Projecteur-leather_shadow_theater3.jpg'),
+array('date' => "0", 'img'=> 'projecteur/Projecteur-leather_shadow_theater3.jpg'),
+array('date' => "1700", 'img'=> 'projecteur/Projecteur-theatre_d\'ombres_chinoises-1.jpg'),
+array('date' => "1600", 'img'=> 'projecteur/Projecteur-theatre_d\'ombres_chinoises-2.jpg'),
 array('date' => "1640", 'img'=> 'projecteur/Projecteur-1640-4.jpg'),
-array('date' => "1999", 'img'=> 'projecteur/Projecteur-1999-LCOS-JVC-8.jpg'),
-array('date' => "", 'img'=> 'projecteur/Projecteur-theatre_d\'ombres_chinoises-2.jpg'),
-array('date' => "", 'img'=> 'projecteur/Projecteur-80_ans_plus-5.jpg'),
-array('date' => "", 'img'=> 'projecteur/Projecteur-mov-la_venir.jpg'),
-array('date' => "", 'img'=> 'projecteur/Projecteur-3LCD-EPSON-8.jpg'),
+array('date' => "1900", 'img'=> 'projecteur/Projecteur-trois_attraper-6.jpg'),
+array('date' => "1930", 'img'=> 'projecteur/Projecteur-80_ans_plus-5.jpg'),
+array('date' => "1985", 'img'=> 'projecteur/Projecteur-mov-la_venir.jpg'),
+array('date' => "1989", 'img'=> 'projecteur/Projecteur-3LCD-EPSON-8.jpg'),
 array('date' => "1996", 'img'=> 'projecteur/Projecteur-1996-DLP-9.jpg'),
-array('date' => "", 'img'=> 'projecteur/Projecteur-theatre_d\'ombres_chinoises-1.jpg'),
 array('date' => "1989", 'img'=> 'projecteur/Projecteur_LCD-1989-EPSON_VJP-2000-7.jpg'),
-array('date' => "1999", 'img'=> 'projecteur/Projecteur-1999-LCOS-JVC-10.jpg'),
+array('date' => "1999", 'img'=> 'projecteur/Projecteur-1999-LCOS-JVC-8.jpg'),
+#array('date' => "1999", 'img'=> 'projecteur/Projecteur-1999-LCOS-JVC-10.jpg'),
 );
 
 $keyimg[11] = 'virtual_reality/08-AlphaBino.2005-WA.jpg';
--- lib/pages.php
+++ lib/pages.php
@@ -12,10 +12,12 @@ function img_url ($item, $opt) {
 
 function fmt_item ($item, $opt=0) {
 	if (!is_array($item)) return '<div class="empty"></div>';
-	$rv='<div>';
+	$rv='<div class="c">';
 	$rv.= '<a href="'.fmt_url($item).'"><img alt="" src="'.img_url($item, $opt).'" /></a>';
-	if ($opt &1) 
-		$rv.='<div class="osd">'.$item['date'].' - '.$item['title'].'</div>';
+	if ($opt &2)
+		$rv.='<div class="osd date">'.$item['date'].'</div>';
+	if ($opt &1)
+		$rv.='<div class="osd title">'.$item['img'].'</div>';
 	$rv.='</div>';
 	return $rv;
 }
@@ -35,7 +37,7 @@ function vis_imagegroup2D ($d) {
 	echo '<div class="container">'.NL;
 	echo '<table class="xmatrix">'.NL;
 	echo '<tr><td class="lr2 tb"></td><td class="lr1 tb"></td><td class="tb">'.gimg($d[0],0).'</td><td class="lr1 tb"></td><td class="lr2 tb"></td></tr>'.NL;
-	echo '<tr><td class="lr2">'.gimg($d[1], 0).'</td><td class="lr1">'.gimg($d[1], 1).'</td><td>'.gimg($d[1], 2, 1).'</td><td class="lr1">'.gimg($d[1], 3).'</td><td class="lr2">'.gimg($d[1], 4).'</td></tr>'.NL;
+	echo '<tr><td class="lr2">'.gimg($d[1], 0).'</td><td class="lr1">'.gimg($d[1], 1, 2).'</td><td>'.gimg($d[1], 2, 3).'</td><td class="lr1">'.gimg($d[1], 3, 2).'</td><td class="lr2">'.gimg($d[1], 4).'</td></tr>'.NL;
 	echo '<tr><td class="lr2 tb"></td><td class="lr1 tb"></td><td class="tb">'.gimg($d[2],0).'</td><td class="lr1 tb"></td><td class="lr2 tb"></td></tr>'.NL;
 	echo '</table>'.NL;
 
