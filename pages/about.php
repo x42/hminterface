302f2dbdc1f4351177e835d3489db3005b67da7f
Robin Gareus <robin@gareus.org>
2012-01-16 00:22:59 +0100

    updated about text.

--- pages/about.php
+++ pages/about.php
@@ -17,22 +17,4 @@ Ces systèmes interfacés sont eux-mêmes des interfaces, des objets communicant
 </p><p><em>
 Développé dans le cadre du cours « IHM et conception des objets communicants » par les étudiants du Master NET de l’université Paris 8, sous la direction de Maurice Benayoun, ce projet en construction est ouvert à toutes les contributions su sceptibles d’enrichir la base de données et les analyses qui lui donnent sens.</em>
 </p>
-
-<h2>Interfaces Homme-Machine.</h2>
-<p>
-Ce site - comme tout site - est une interface.
-</p><p>
-Pris dans une boucle récursive, son propos est d'offrir un outil de stockage, d'organisation logique et de réflexion sur l'évolution des IHM - Interfaces Homme Machine - d'un point de vue anthropocentré.
-</p><p>
-De quelles fonctions, de quels organes, de quels besoins découlent nos interfaces "artificielles" modernes ? Quelles mutations, quelles évolutions, quelles avancées ont amené l'Homme à construire nos outils modernes ?
-</p><p>
-Outils : voilà ce que sont ces machines. Outils, moyens : non des fins en soi. La Machine ne communique pas avec l'Homme. L'Homme communique avec son semblable à travers la Machine.
-</p><p>
-La Machine de nos IHM est ainsi elle-même une interface, un objet communiquant : elle permet de transmettre des messages aux êtres humains.
-</p><p>
-De ce point de vue, nos IHM - Interfaces Homme Machine - sont alors des IHH - Interfaces Homme - Homme - via la Machine. A l'instar de ce site&hellip;
-</p>
-
-
-
 </div>
